d4793389ea2fdd0bc18b1ad6d64af607e002fbcd
Raul Perez <zerepangel@gmail.com>
2017-02-08 22:24:07 +0100

    Third Tutorial on React

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,27 @@ import React from 'react';
 // State function:
 class App extends React.Component {
   render(){
-    return <h1 >Hello worldieron </h1>
-    //React.createElement('h1', null, 'hello guys')
+    // Can only return 1 element so we put everything under a div
+    return (
+      <div>
+        <h1>Hello worldieron </h1> 
+        <b>Bold</b>
+      </div>
+    )
   }
 }
+
+
+export default App; 
+
+
+
+
+
+
+// Useful things to remember
 
 // Stateless function: 
 //const App = () => <h1 >Hello Stateless </h1>
 
-export default App; 
+//React.createElement('h1', null, 'hello guys') =  <h1>Hello worldieron </h1> 

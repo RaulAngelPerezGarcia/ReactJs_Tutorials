179aaae7087bd382e7cb449b79d38b32de26c881
Raul Perez <zerepangel@gmail.com>
2017-02-08 22:17:04 +0100

    First Commit

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// State function:
+class App extends React.Component {
+  render(){
+    return <h1 >Hello worldieron </h1>
+    //React.createElement('h1', null, 'hello guys')
+  }
+}
+
+// Stateless function: 
+//const App = () => <h1 >Hello Stateless </h1>
+
+export default App;

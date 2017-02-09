96c3b23e677e9b932a7fbb39f28e6971b6f184e5
Raul Perez <zerepangel@gmail.com>
2017-02-09 21:12:47 +0100

    Tutorial 9: Normalize events with reacts synthetic event system

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,49 @@
 import React from 'react';
 
 class App extends React.Component{
+
+	constructor(){
+		super();
+		this.state = {currentEvent: "---"}
+		this.update = this.update.bind(this)
+	}
+
+	update(e){
+		this.setState({currentEvent: e.type})
+	}
+
+	render(){
+		return (
+			<div>
+				<textarea
+					onKeyPress={this.update}
+					onCopy={this.update}
+					onCut={this.update}
+					onPaste={this.update}
+					onFocus={this.update}
+					onBlur={this.update}
+					onDoubleClick={this.update}
+					onTouchStart={this.update}
+					onTouchMove={this.update}
+					onTouchEnd={this.update}
+					cols="30"
+					rows="10" />
+
+				<h1>{this.state.currentEvent}</h1>
+			</div>
+		)
+	}
+}
+
+export default App; 
+
+
+
+
+
+/* 
+Tutorial 8:
+class App extends React.Component{
 	render(){
 		return <Title text="This is the text"/>
 	}
@@ -21,13 +64,6 @@ Title.propTypes = {
 }
 
 
-export default App; 
-
-
-
-
-
-/* 
 Tutorial 7:
 class App extends React.Component {
 	render(){

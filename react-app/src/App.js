114435efa5ee298e6a88a8f447ab1ce4736a1f47
Raul Perez <zerepangel@gmail.com>
2017-02-15 16:39:57 +0100

    Tutorial 14: Use map to create react components from arrays of data

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+//import ReactDOM from 'react-dom'
 
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state = {items: []}
+	}
+
+	componentWillMount(){
+		fetch( 'https://swapi.co/api/people/?format=json' )
+      .then( response => response.json() )
+      .then( ({results: items}) => this.setState({items}))
+	}
+
+	filter(e){
+		this.setState({filter: e.target.value})
+	}
+
+	render(){
+		let items = this.state.items;
+
+		if(this.state.filter){
+			items = items.filter(item =>item.name.toLowerCase().includes(this.state.filter.toLowerCasse()))
+		}
+
+		return(
+			<div>
+				<input type="text" 
+				onChange={this.filter.bind(this)}/>
+				{items.map(item =>
+				<Person key={item.name} person={item} />)}
+			</div>
+		)
+	}
+}
+
+const Person = (props) =>  <h4>{props.person.name}</h4>
+
+export default App
+
+
+/* 
+Tutorial 12:
 class App extends React.Component{
 	constructor(){
 		super();
@@ -36,11 +77,6 @@ class App extends React.Component{
 }
 
 App.defaultProps = {val:0}
-
-export default App
-
-
-/* 
 Tutorial 11:
 class App extends React.Component{
 	constructor(){

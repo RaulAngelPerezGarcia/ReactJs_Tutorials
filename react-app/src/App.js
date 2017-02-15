95136f32f1da9a9155c72e382ad1f09e2b7cc41e
Raul Perez <zerepangel@gmail.com>
2017-02-15 16:17:21 +0100

    Tutorial 11: Control react component updates when new props are received

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,47 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 
+class App extends React.Component{
+	constructor(){
+		super();
+		this.state = {increasing: false}
+	}
+	
+	update(){
+		ReactDOM.render(
+			<App val={this.props.val+1} />,
+			document.getElementById('root'))
+	}
+
+	componentWillReceiveProps(nextProps){
+		this.setState({increasing: nextProps.val > this.props.val})
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		return nextProps.val % 5 === 0;
+	}
+
+	render(){
+		console.log(this.state.increasing)
+		return(
+			<button onClick={this.update.bind(this)}>
+			{this.props.val}
+			</button>
+		)
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		console.log(`prevProps: ${prevProps.val}`)
+	}
+}
+
+App.defaultProps = {val:0}
+
+export default App
+
+
+/* 
+Tutorial 11:
 class App extends React.Component{
 	constructor(){
 		super();
@@ -56,12 +97,6 @@ class Wrapper extends React.Component{
 }
 
 export default Wrapper; 
-
-
-
-
-
-/* 
 Tutorial 10:
 class App extends React.Component{
 
